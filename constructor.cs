6ce2a1cc9c426e8761ab64a55464845850540440
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-08-09 11:43:51 -0400

    used ctor using parameters.

---
+++ constructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        //called object
+        Test ford = new Test("toyato", "red", 1999);
+        Console.WriteLine(ford.color + " "+ ford.model + " " + ford.year);
+    }
+}
+class Test
+{
+    public string model;
+    public string color;
+    public int year;
+    public Test(string modelName,string Color,int year)// create constructor 
+    {
+        model = modelName;
+        color = Color;
+        this.year = year;
+    }
+}
