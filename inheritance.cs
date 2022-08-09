9ada0cfa49e451326e88cf244903976644c7c9a7
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-08-09 16:10:06 -0400

    Add files via upload

---
+++ inheritance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        test Test = new test();
+
+        Test.name = "ford";
+        Test.year = 1115;
+        Test.model();
+        //Console.WriteLine(Test.model);
+       
+        Console.WriteLine(Test.name + " " + Test.year);
+    }
+}
+class car//parent class
+{
+    public string name;
+    public void model()//method
+     {
+        Console.WriteLine("toyota");
+    }
+   
+}
+class test : car //dervied class
+{
+    public string color;
+    public int year;
+}
