557f9aa73086c4f36c46dd7e71b642936f7af0f5
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-08-09 16:16:27 -0400

    Add files via upload

---
+++ sealedkeyword.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        test2 Test = new test2();
+
+        //Test.name = "ford";
+        Test.year = 1115;
+        Test.number = "5646";
+        //Test.model();
+        //Console.WriteLine(Test.model);
+       
+        Console.WriteLine( Test.year + " " + Test.number);
+    }
+}
+sealed class car//sealed keyword
+{
+    public string name;
+    public void model()//method
+     {
+        Console.WriteLine("toyota");
+    }
+   
+}
+class test //dervied class
+{
+    public string color;
+    public int year;
+}
+class test2 : test
+{
+    public string number;
+}
