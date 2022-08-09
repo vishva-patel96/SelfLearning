8c0d9ea8b02a97389e06739db620e8211f0facf3
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-08-09 15:42:09 -0400

    used private keyword

---
+++ encapsulation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        //called object
+        Test test= new Test();
+        test.Name = "ford";
+        Console.WriteLine(test.Name);
+    }
+}
+class Test
+{
+    
+    private string name;// access modifiers
+    
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+   
+}
