eb794200324229126ac35a0db37a718d06af51f7
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-08-12 10:26:50 -0400

    implemented virtual and override keyword

---
+++ Polymorphism.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Animal pig = new Pig();
+        pig.animalsound();
+
+        Animal cow = new Cow(); 
+        cow.animalsound();
+    }
+}
+class  Animal
+{
+     public virtual void animalsound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+   
+}
+
+class Pig : Animal
+{
+    public override void animalsound()
+    {
+        Console.WriteLine("weee weee");
+    }
+
+
+}
+class Cow : Animal
+{
+    public override void animalsound()
+    {
+        Console.WriteLine("bow bow");
+    }
+
+
+
+}
