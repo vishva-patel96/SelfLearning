6cb6921cddd300448ca68683d774dabafcd15df4
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-08-12 09:47:51 -0400

    Update inheritance.cs

--- inheritance.cs
+++ inheritance.cs
@@ -3,32 +3,62 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        test2 Test = new test2();
+        Test test = new Test("cow", "black",55);
 
-        Test.name = "ford";
-        Test.year = 1115;
-        Test.number = "5646";
-        Test.model();
-        //Console.WriteLine(Test.model);
-       
-        Console.WriteLine(Test.name + " " + Test.year + " " + Test.number);
+        test.wheel.printWheelBrand();
     }
 }
-class car//parent class
+class car
 {
-    public string name;
-    public void model()//method
-     {
-        Console.WriteLine("toyota");
-    }
+    public string model;
+    public void test2()
+    {
+        Console.WriteLine("");
+    }    
    
 }
-class test : car //dervied class
+
+class Wheels
 {
+    public int numberOfWheels;
+    public string brand;
+
+    public Wheels(int numberOfWheels, string brand)
+    {
+        this.numberOfWheels = numberOfWheels;
+        this.brand = brand;
+    }
+
+    public void printNumberofWheels()
+    {
+        Console.WriteLine("No of wheels:" + this.numberOfWheels);
+    }
+
+    public void printWheelBrand()
+    {
+        Console.WriteLine("wheels brand:" + this.brand);
+    }
+
+}
+class Test:car//dervied class
+{
+    
+    public string name;
     public string color;
     public int year;
-}
-class test2 : test
-{
-    public string number;
+    public Wheels wheel;
+   
+    public Test(string Name, string Color, int Year)//method
+    {
+        name = Name;
+        color = Color;
+        year = Year;
+        wheel = new Wheels(4, "michelin");
+        
+     
+
+    }
+
+
+
 }
