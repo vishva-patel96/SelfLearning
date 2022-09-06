82bbb06b8fa41d49f00d3acec02d255283759c35
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-09-06 11:42:08 -0400

    used IsEmpty operation in LinkedList.

---
+++ Linklist(IsEmpty).cs
@@ -0,0 +1,35 @@
+﻿public class Linklist
+{
+    public class Node
+    {
+        public int data;
+        public Node nextelement;
+
+        public Node()
+        {
+            nextelement = null;
+        }
+    };
+    Node head;
+
+    public Linklist()
+    {
+        head = null;
+
+    }
+    public bool IsEmpty()
+    {
+        if (head == null)
+            return true;
+        else
+            return false;
+    }
+}
+public class Mainclass
+{
+    public static void Main (string[] args)
+    {
+        Linklist list = new Linklist();
+        Console.WriteLine(list.IsEmpty());
+    }
+}
